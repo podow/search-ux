234a092341d4f33cd9b8aefac8f6e3095507494f
Berdnickov Bogdan <podowko@gmail.com>
2016-03-14 15:11:34 +0300

    commit

---
+++ common.js
@@ -0,0 +1,30 @@
+var	formSearch = document.querySelector('.search'),
+		inputSearch = formSearch.children[0].children[0],
+		btnSearch = formSearch.children[0].children[1];
+
+var query = "";
+
+setInterval(function () {
+	if (!(inputSearch.value == "")){
+		formSearch.classList.add('search-active');
+	} else {
+		formSearch.classList.remove('search-active');
+	}}, 100);
+
+if (window.getSelection){
+	setInterval(function () {
+		query = window.getSelection().toString();
+		inputSearch.value = query;
+	}, 100);
+	btnSearch.addEventListener('click', function () {
+		window.open('http://ru.wikipedia.org/wiki/' + query);
+	});
+} else if (document.selection){
+	setInterval(function () {
+		query = document.selection.createRange().text;
+		inputSearch.value = query;
+	}, 100);
+	btnSearch.addEventListener('click', function () {
+		window.open('http://ru.wikipedia.org/wiki/' + query);
+	});
+}
